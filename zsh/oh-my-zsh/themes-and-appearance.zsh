830f16dd36a008cab4e625911aa7871c9d5c362f
Jon Lai <jonathan@jonlai.com>
2018-01-27 18:17:49 -0500

    Add LS_COLORS for ubuntu support

--- zsh/oh-my-zsh/themes-and-appearance.zsh
+++ zsh/oh-my-zsh/themes-and-appearance.zsh
@@ -1,5 +1,6 @@
 autoload -U colors && colors
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
+export LS_COLORS="di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
 
 if [[ "$OSTYPE" == darwin* ]]; then
   ls -G . &>/dev/null && alias ls='ls -G'
